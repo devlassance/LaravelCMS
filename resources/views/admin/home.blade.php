c91b62888364eeb3a0823662de4cb52234add973
Jorge <lassancejorgeluiz@gmail.com>
2021-10-07 19:03:18 -0300

    Configuraçoes inciais adminlt

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,15 @@
-<h1>Admin</h1>
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+    Conteudo...s
+@endsection
+
+@section('css')
+    <!-- Importações de css e o mesmo para javascript, trocando css por js -->
+@endsection
